67321527550f780a9bfc6bc4e866cca8e6c37360
Saena <toastyhatesthesea@gmail.com>
2021-12-28 04:08:41 -0800

    Fleshed out parseData() function and created dataMover() function

--- src/Hydrothermal.java
+++ src/Hydrothermal.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Hydrothermal
 {
+
+    HydroBoard aHydroBoard;
+
+    public Hydrothermal(int size)
+    {
+        aHydroBoard = new HydroBoard(size);
+    }
+
+
+
     public void parseData(File aFile) throws FileNotFoundException
     {
         Scanner aFileScanner = new Scanner(aFile);
@@ -16,10 +26,42 @@ public class Hydrothermal
             while (aLineScan.hasNext())
             {
                 String aLine = aLineScan.next();
-                Scanner firstData = new Scanner(aLine);
+                Scanner dataScanner = new Scanner(aLine);
+                dataScanner.useDelimiter(",");
+
+                String x1 = dataScanner.next();
+                String y1 = dataScanner.next();
+                y1 = y1.strip();
+
+                aLine = aLineScan.next();
+                dataScanner = new Scanner(aLine);
+                dataScanner.useDelimiter(",");
+
+                String x2 = dataScanner.next();
+                x2 = x2.strip();
+                String y2 = dataScanner.next();
+
+                dataMover(x1, y1, x2, y2);
+
             }
         }
 
+    }
+
+    public void dataMover(String x1, String y1, String x2, String y2)
+    {
+        int x1Int = Integer.parseInt(x1);
+        int y1Int = Integer.parseInt(y1);
+        int x2Int = Integer.parseInt(x2);
+        int y2Int = Integer.parseInt(y2);
+
+        if (x1Int == x2Int)
+        {
+            if (y1Int < y2Int)
+            {
+                aHydroBoard.parseRow(x1Int, y1Int, y2Int);
+            }
+        }
     }
 
 }
@@ -69,7 +111,7 @@ class HydroTesters
 {
     public static void main(String[] asdasdasdsad) throws FileNotFoundException
     {
-        Hydrothermal hydro = new Hydrothermal();
+        Hydrothermal hydro = new Hydrothermal(1000);
         hydro.parseData(new File("hydro_large"));
 
     }
