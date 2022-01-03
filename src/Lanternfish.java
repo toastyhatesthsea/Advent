800cc20fd303b5147a40a6246f06b72639dd680b
Zafaripour <toastyhatesthesea@gmail.com>
2022-01-02 22:07:29 -0800

    Finished LanternFish class and passed all tests

--- src/Lanternfish.java
+++ src/Lanternfish.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Lanternfish
@@ -21,7 +22,17 @@ public class Lanternfish
             aFishKey = aFishKey.strip();
             someFish.addValue(aFishKey);
         }
+    }
 
+    /**
+     * Runs the simulation according to the amount of days
+     */
+    public void runSimulation()
+    {
+        for (int i = 0; i < amountOfDaysToSimulate; i++)
+        {
+            someFish.processDay();
+        }
     }
 
     public Lanternfish(int amountOfDays)
@@ -35,7 +46,7 @@ public class Lanternfish
 
 class Fish
 {
-    HashMap<String, Integer> amountOfFish;
+    HashMap<String, Long> amountOfFish;
 
     public Fish()
     {
@@ -44,12 +55,13 @@ class Fish
 
     /**
      * Returns true if value is found
+     *
      * @param aKey String
      * @return boolean
      */
     public boolean addValue(String aKey)
     {
-        Integer currentValue = amountOfFish.putIfAbsent(aKey, 1);
+        Long currentValue = amountOfFish.putIfAbsent(aKey, 1L);
 
         if (currentValue != null)
         {
@@ -58,13 +70,65 @@ class Fish
         }
         return false;
     }
+
+    public void processDay()
+    {
+        HashMap<String, Long> tempFishes = new HashMap<>();
+        int sizeOfMap = amountOfFish.size();
+        for (int i = 0; i <= 8; i++)
+        {
+            String aKey = Integer.toString(i);
+
+            Long aValue = amountOfFish.get(aKey);
+
+            if (aValue != null)
+            {
+                if (aKey.equals("0"))
+                {
+                    tempFishes.put("8", aValue);
+
+                    Long tempValue = tempFishes.putIfAbsent("6", aValue);
+
+                    if (tempValue != null)
+                    {
+                        tempFishes.put("6", tempValue + aValue);
+                    }
+                    amountOfFish.remove("0");
+                } else
+                {
+                    String currentKey = Integer.toString(i - 1);
+                    Long value = tempFishes.putIfAbsent(currentKey, aValue);
+
+                    if (value != null)
+                    {
+                        tempFishes.put(currentKey, aValue + value);
+                    }
+
+                    amountOfFish.remove(aKey);
+                }
+            }
+        }
+        amountOfFish.putAll(tempFishes);
+    }
+
+    public long amountOfCurrentFish()
+    {
+        long answer = 0;
+        for (Map.Entry<String, Long> anEntry : amountOfFish.entrySet())
+        {
+            answer += anEntry.getValue();
+        }
+        return answer;
+    }
 }
 
 class Testers
 {
     public static void main(String[] asdasdasdasd) throws FileNotFoundException
     {
-        Lanternfish aLatern = new Lanternfish(80);
+        Lanternfish aLatern = new Lanternfish(256);
         aLatern.parseData(new File("latern_large"));
+        aLatern.runSimulation();
+        long answer = aLatern.someFish.amountOfCurrentFish();
     }
 }
